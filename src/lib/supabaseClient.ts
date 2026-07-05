b6f3c5318bb358c41c7f8c2daec50828b59a8118
Eddy-Dja <djahouiedgard@gmail.com>
2026-07-05 12:10:30 +0200

    Fix connexion Supabase pour Cloudflare

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://fzqbraitdhilveyrwuew.supabase.co';
+const supabaseAnonKey = 'sb_publishable_kpdhtVUBNYp13POEz4Jj1Q_b34-3ZLY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
